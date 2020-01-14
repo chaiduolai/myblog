7ed6c6414115471df9024191cfabd4ddffd9db58
chaiduolai <cduolai@163.com>
2020-01-14 18:01:32 +0800

    线上第一版

--- vue_mogu_admin/config/prod.env.js
+++ vue_mogu_admin/config/prod.env.js
@@ -1,18 +1,15 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  
+
     //生产环境
-    ADMIN_API: '"http://101.132.194.128:8601"',
-    PICTURE_API: '"http://101.132.194.128:8602"',
-    WEB_API: '"http://101.132.194.128:8603"',
-    SOLR_API: '"http://101.132.194.128:8080/solr"',
-
+    ADMIN_API: '"http://47.101.195.138:8601"',
+    PICTURE_API: '"http://47.101.195.138:8602"',
+    WEB_API: '"http://47.101.195.138:8603"',
+    SOLR_API: '"http://47.101.195.138:8080/solr"',
     // BASE_IMAGE_URL: '"http://101.132.194.128:8600"',
-    BASE_IMAGE_URL: '"http://picture.moguit.cn"',
-
-
-    BLOG_WEB_URL: '"http://101.132.194.128:9527"',  
+    BASE_IMAGE_URL: '"http://picture.cduolai.com"',
+    BLOG_WEB_URL: '"http://www.cduolai.com:9527"',
 
     //生产环境
     // ADMIN_API: '"http://101.132.194.128:8080/mogu_admin"',
@@ -20,7 +17,4 @@ module.exports = {
     // WEB_API: '"http://101.132.194.128:8080/mogu_web"',
     // SOLR_API: '"http://101.132.194.128:8080/solr"',
     // BASE_IMAGE_URL: '"http://101.132.194.128:8600"',
-
-    BLOG_WEB_URL: '"http://www.moguit.cn"',
-    
 }
--- vue_mogu_web/config/prod.env.js
+++ vue_mogu_web/config/prod.env.js
@@ -1,15 +1,15 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  
+
   //配置线上环境
   // BASE_BLOG_API: '"http://101.132.194.128:9527"',
   // PICTURE_API: '"http://101.132.194.128:8602"',
   // WEB_API: '"http://101.132.194.128:8603"',
 
   //配置线上环境
-  WEB_API: '"http://101.132.194.128:8603"',
+  WEB_API: '"http://47.101.195.138:8603"',
   // PICTURE_HOST: '"http://101.132.194.128:8600"',
-  PICTURE_HOST: '"http://picture.moguit.cn"',
+  PICTURE_HOST: '"http://picture.cduolai.com"',
 
 }
